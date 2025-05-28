25b9742cc5a38bc8b025e63b8f6885b319652ffe
Francisco Das Chagas <66851227+francisco-das-chagas@users.noreply.github.com>
2025-05-27 23:53:07 -0300

    estilizando login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,35 +1,33 @@
 import { InputText } from 'primereact/inputtext';
 import { IconField } from 'primereact/iconfield';
 import { InputIcon } from 'primereact/inputicon';
-import { Button } from 'primereact/button';
-        
-        
+import { Button } from 'primereact/button';        
 
 const Login = () => {
   return ( 
     <>
-        <div>
-            <form>
-              <label htmlFor="email">Email</label>
-              <InputText 
-                id='email'
-                type='email'
-                placeholder='email@email.com'
+      <div  className='h-screen flex align-items-center justify-content-center'>
+          <form className='surface-0'>
+            <label htmlFor="email">Email</label>
+            <InputText 
+              id='email'
+              type='email'
+              placeholder='email@email.com'
+            />
+            <label htmlFor="senha">Senha</label>
+            <IconField>
+              <InputIcon className="pi pi-eye" />
+              <InputText
+                id='senha'
+                placeholder='********'
               />
-              <label htmlFor="senha">Senha</label>
-              <IconField>
-                <InputIcon className="pi pi-eye" />
-                <InputText
-                  id='senha'
-                  placeholder='********'
-                />
-              </IconField>
-              <Button
-              label="Entrar"
-              type='submit'
-              />
-            </form>
-        </div>
+            </IconField>
+            <Button
+            label="Entrar"
+            type='submit'
+            />
+          </form>
+      </div>
     </>
    );
 }
