188a1bed52c8292f7e82cdbb3fa232ddee153862
Francisco Das Chagas <66851227+francisco-das-chagas@users.noreply.github.com>
2025-05-28 00:15:17 -0300

    estilização finalizada

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,31 +2,38 @@ import { InputText } from 'primereact/inputtext';
 import { IconField } from 'primereact/iconfield';
 import { InputIcon } from 'primereact/inputicon';
 import { Button } from 'primereact/button';
-import "primereact/resources/themes/lara-dark-indigo/theme.css"
+import "primereact/resources/themes/soho-light/theme.css"
 
 const Login = () => {
   return ( 
     <>
-      <div  className='bg-primary-500 h-screen flex align-items-center justify-content-center'>
-          <form className='surface-0 p-3'>
-            <h3>Seja Bem-vindo</h3>
-            <label htmlFor="email">Email</label>
+      <div className='bg-primary-800 h-screen flex align-items-center justify-content-center px-3'>
+          <form className='col-12 md:col-3 surface-0 p-3 border-round-md '>
+            <h3 className='text-center text-4xl'>Seja Bem-vindo</h3>
+            <label htmlFor="email" className='block uppercase font-bold text-sm mb-1'>Email</label>
             <InputText 
               id='email'
               type='email'
               placeholder='email@email.com'
+              className='mb-3 w-full'
             />
-            <label htmlFor="senha">Senha</label>
-            <IconField>
-              <InputIcon className="pi pi-eye" />
-              <InputText
-                id='senha'
-                placeholder='********'
-              />
-            </IconField>
+            <label htmlFor="senha" className='block uppercase font-bold text-sm mb-1'>Senha</label>
+            
+            <div className='mb-3'>
+              <IconField>
+                <InputIcon className="pi pi-eye" />
+                <InputText
+                  id='senha'
+                  placeholder='********'
+                  className='w-full'
+                />
+              </IconField>
+            </div>
+            
             <Button
             label="Entrar"
             type='submit'
+            className='w-full'
             />
           </form>
       </div>
