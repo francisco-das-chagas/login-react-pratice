4bf3aa2210ae492ef5c19951773140f0245f9d56
Francisco Das Chagas <66851227+francisco-das-chagas@users.noreply.github.com>
2025-05-29 00:16:12 -0300

    adicionando requisição

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,15 +4,22 @@ import { InputIcon } from 'primereact/inputicon';
 import { Button } from 'primereact/button';
 import "primereact/resources/themes/soho-light/theme.css"
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
 
-  const [mostrarSenha, setMostrarSenha] = useState(false)
+  const [mostrarSenha, setMostrarSenha] = useState(false);
+
+  const {register, handleSubmit} = useForm();
+
+  function logar(dados) {
+    console.log(dados);
+  }
 
   return ( 
     <>
       <div className='bg-primary-800 h-screen flex align-items-center justify-content-center px-3'>
-          <form className='col-12 md:col-3 surface-0 p-3 border-round-md '>
+          <form onSubmit={handleSubmit(logar)} className='col-12 md:col-3 surface-0 p-3 border-round-md '>
             <h3 className='text-center text-4xl'>Seja Bem-vindo</h3>
             <label htmlFor="email" className='block uppercase font-bold text-sm mb-1'>Email</label>
             <InputText 
@@ -20,6 +27,7 @@ const Login = () => {
               type='email'
               placeholder='email@email.com'
               className='mb-3 w-full'
+              {...register('email', {required: true})}
             />
             <label htmlFor="senha" className='block uppercase font-bold text-sm mb-1'>Senha</label>
             
@@ -34,6 +42,7 @@ const Login = () => {
                   type={ mostrarSenha ? 'text' : 'password'}
                   placeholder='********'
                   className='w-full'
+                  {...register('senha', {required: true})}
                 />
               </IconField>
             </div>
