e6d5b5b3a84a3514d5f35172abb51467d37f9162
Francisco Das Chagas <66851227+francisco-das-chagas@users.noreply.github.com>
2025-05-29 23:29:27 -0300

    Prime React e React Context

--- src/routes/Paths.jsx
+++ src/routes/Paths.jsx
@@ -1,17 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "../pages/Login";
+import Home from "../pages/Home";
 
 const Paths = () => {
   return ( 
     <>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Login/>}/>
+            <Route path="/" element={<Login />}/>
+            <Route path="/home" element={<Home />}/>
           </Routes>
         </BrowserRouter>
     </>
 
-   );
+   )
 }
  
 export default Paths;
